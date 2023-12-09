b1117b60eec9b588fc87fd6b6eb6e4f727c6b774
AmarinoLama <amarinolama@gmail.com>
2023-12-09 22:09:20 +0100

    fix (randomizer, sendSongsVLC) : poder mandar otras listas

--- src/randomizer.py
+++ src/randomizer.py
@@ -1,3 +1,4 @@
+from searchSongs import searchSongs
 import random
 
 def randomizer(array):
@@ -18,7 +19,8 @@ def randomizer(array):
 
 if __name__ == "__main__":
 
-    print(randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]))
-    assert randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]) != {'cancion1':1,'cancion2':2,'cancion3':3,'cancion4':4,'cancion5':5}
-    assert randomizer(1) == None
-    assert randomizer([]) == None
+    #print(randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]))
+    #assert randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]) != {'cancion1':1,'cancion2':2,'cancion3':3,'cancion4':4,'cancion5':5}
+    #assert randomizer(1) == None
+    #assert randomizer([]) == None
+    print(randomizer(searchSongs()))
--- src/sendSongsVLC.py
+++ src/sendSongsVLC.py
@@ -1,5 +1,13 @@
+from randomizer import randomizer
 from searchSongs import searchSongs
+import subprocess
 
-print("====================")
-print(searchSongs())
-print(len(searchSongs()))
+def sendSongsVLC():
+    if len(searchSongs()) > 1:
+        print("Ha funcionado")
+        subprocess.run("cd C:\Program Files\VideoLAN\VLC", shell=True)
+    else:
+        print("No se ha encontrado ninguna canción")
+        return None
+    
+print(sendSongsVLC())
