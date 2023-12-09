4de6f3643192d44a98c86fc830b1209811ff12bc
AmarinoLama <amarinolama@gmail.com>
2023-12-09 18:32:41 +0100

    fix (findXSPF.py) : ahora ya puede encontrar el xspf

--- src/findXSPF.py
+++ src/findXSPF.py
@@ -1,10 +1,27 @@
 import os
 
-def findXml():
+# el xspf esta dentro de la carpeta musica
+# el xspf esta dentro del proyecto
+
+def findXSPF():
+
     filesInDirectory = os.listdir(os.getcwd())
     correctFile = [file for file in filesInDirectory if file.endswith('xspf')]
-    finalFile = correctFile
-    return finalFile
 
-print(findXml())
-print(os.getcwd())
+    if len(correctFile) == 0:
+        userName = os.getenv('USERNAME')
+        ruta_music = r'C:\\Users\\' + userName + r'\\Music'
+        filesInDirectory = os.listdir(ruta_music)
+        correctFile = [file for file in filesInDirectory if file.endswith('xspf')]
+        if len(correctFile) == 0:
+            return "No se encontró el xspf"
+        elif len(correctFile) == 1:
+            return correctFile
+        elif len(correctFile) > 1:
+            return correctFile[0]
+    elif len(correctFile) == 1:
+        return correctFile
+    elif len(correctFile) > 1:
+        return correctFile[0]
+
+print(findXSPF())
