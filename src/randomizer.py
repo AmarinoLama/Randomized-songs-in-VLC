ff2a8e3f978cc54dbea9df94f10001eeab67ad7d
AmarinoLama <amarinolama@gmail.com>
2023-12-09 17:52:08 +0100

    feat : randomizer.py añadido

---
+++ src/randomizer.py
@@ -0,0 +1,24 @@
+import random
+
+def randomizer(array):
+    
+    try:
+        assert isinstance(array,list)
+    except AssertionError:
+        print("No ha llegado a randomizer una lista de canciones")
+        return None
+
+    if array != []:
+        listB = [x + 1 for x in range(len(array))]
+        random.shuffle(listB)
+        return dict(zip(array,listB))
+    else:
+        print("La lista de las canciones está vacía")
+        return None
+
+if __name__ == "__main__":
+
+    print(randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]))
+    assert randomizer(["cancion1","cancion2","cancion3","cancion4","cancion5"]) != {'cancion1':1,'cancion2':2,'cancion3':3,'cancion4':4,'cancion5':5}
+    assert randomizer(1) == None
+    assert randomizer([]) == None
