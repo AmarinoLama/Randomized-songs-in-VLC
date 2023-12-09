d692b6a43f2ec4fc1e81c71a0c8b0c89bd3cd3c2
AmarinoLama <amarinolama@gmail.com>
2023-12-09 20:14:46 +0100

    refactor : ahora el findXSPF devuelve la ruta

--- src/findXSPF.py
+++ src/findXSPF.py
@@ -7,23 +7,19 @@ def findXSPF():
 
     filesInDirectory = os.listdir(os.getcwd())
     correctFile = [file for file in filesInDirectory if file.endswith('xspf')]
+    userName = os.getenv('USERNAME')
 
     if len(correctFile) == 0:
-        userName = os.getenv('USERNAME')
         ruta_music = r'C:\\Users\\' + userName + r'\\Music'
         filesInDirectory = os.listdir(ruta_music)
         correctFile = [file for file in filesInDirectory if file.endswith('xspf')]
         if len(correctFile) == 0:
             print("No se encontró el xspf")
             return None
-        elif len(correctFile) == 1:
-            return correctFile
-        elif len(correctFile) > 1:
-            return correctFile[0]
-    elif len(correctFile) == 1:
-        return correctFile
-    elif len(correctFile) > 1:
-        return correctFile[0]
+        elif len(correctFile) >= 1:
+            return ruta_music + correctFile[0]
+    elif len(correctFile) >= 1:
+        return r"C:\\Users\\"+ userName + r"\\Desktop\\proyectoVLC\\Randomized-songs-in-VLC\\" + correctFile[0]
 
 if __name__ == "__main__":
 
